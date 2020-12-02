f04799075b37e2a03b5dc9cb194448139f3d70f0
mrdotx <klassiker@gmx.de>
2020-12-02 10:57:22 +0100

    pacman.log added

--- fzf_aur.sh
+++ fzf_aur.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/fzf/fzf_aur.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/fzf
-# date:       2020-11-05T14:02:31+0100
+# date:       2020-12-02T10:50:53+0100
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script to install/remove packages from aur
@@ -21,15 +21,19 @@ fi
 
 # menu
 select=$(printf "%s\n" \
-            "1) install packages" \
-            "2) remove installed packages" \
-            "3) remove explicit installed packages" \
-            "4) remove installed packages without dependencies" \
-            "5) remove installed packages from aur" \
-    | fzf -e -i --cycle \
+            "1) show pacman.log in pager" \
+            "2) install packages" \
+            "3) remove installed packages" \
+            "4) remove explicit installed packages" \
+            "5) remove installed packages without dependencies" \
+            "6) remove installed packages from aur" \
+    | fzf -e -i --cycle --preview "grep \"$(tail -n1 /var/log/pacman.log \
+        | cut -d'T' -f1 \
+        | tr -d '\[')\" /var/log/pacman.log \
+        | tac" --preview-window "right:70%" \
 )
 
-# aur package lists
+# packages
 execute() {
     search_options="$1"
     process_options="$2"
@@ -40,19 +44,22 @@ execute() {
 
 # select executables
 case "$select" in
-    "1) install packages")
+    "1) show pacman.log in pager")
+        $PAGER /var/log/pacman.log
+        ;;
+    "2) install packages")
         execute "Slq" "S"
         ;;
-    "2) remove installed packages")
+    "3) remove installed packages")
         execute "Qq" "Rsn"
         ;;
-    "3) remove explicit installed packages")
+    "4) remove explicit installed packages")
         execute "Qqe" "Rsn"
         ;;
-    "4) remove installed packages without dependencies")
+    "5) remove installed packages without dependencies")
         execute "Qqt" "Rsn"
         ;;
-    "5) remove installed packages from aur")
+    "6) remove installed packages from aur")
         execute "Qmq" "Rsn"
         ;;
     *)
