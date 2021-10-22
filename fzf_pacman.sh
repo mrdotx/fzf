de6f428f55454c505acf40385ce35263d7e80c54
mrdotx <klassiker@gmx.de>
2021-10-22 10:29:30 +0200

    rankmirrors added to preview mirrorlist

--- fzf_pacman.sh
+++ fzf_pacman.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/fzf/fzf_pacman.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/fzf
-# date:   2021-10-18T08:57:56+0200
+# date:   2021-10-22T10:27:22+0200
 
 # auth can be something like sudo -A, doas -- or nothing,
 # depending on configuration requirements
@@ -129,6 +129,8 @@ while true; do
                     \"$auth\" pacdiff -f -o
                     ;;
                 6.2*)
+                    rankmirrors -n 0 -t /etc/pacman.d/mirrorlist
+                    printf \"\n\"
                     < \"$pacman_mirrors\"
                     ;;
                 6.1*)
