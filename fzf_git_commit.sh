b117b26b563d99090ac6841de47e7ed27dd5165b
mrdotx <klassiker@gmx.de>
2022-05-18 16:30:40 +0200

    reset option added

--- fzf_git_commit.sh
+++ fzf_git_commit.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/fzf/fzf_git_commit.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/fzf
-# date:   2022-05-18T14:01:41+0200
+# date:   2022-05-18T16:29:42+0200
 
 # help
 script=$(basename "$0")
@@ -22,9 +22,45 @@ git_commit() {
         | cut -d" " -f1
 }
 
+git_commits_reset() {
+    printf "\rDelete all commits in the current respository (YES): " \
+        && read -r "key"
+    case "$key" in
+        YES)
+            printf "1) Create orphan branch...\n"
+            git checkout --orphan latest_branch
+
+            printf "2) Add all the files and folders...\n"
+            git add -A
+
+            printf "3) Commit the changes...\n"
+            git commit -am "reset commits"
+
+            printf "4) Delete the master branch...\n"
+            git branch -D master
+
+            printf "5) Rename the current branch to master...\n"
+            git branch -m master
+
+            printf "6) Force update repository...\n"
+            git push -f origin master
+
+            printf "7) Set upstream...\n"
+            git push --set-upstream origin master
+            ;;
+        *)
+            printf "No commits deleted...\n"
+            exit 0
+            ;;
+    esac
+}
+
 case "$1" in
     -h | --help | "")
         printf "%s\n" "$help"
+        ;;
+    --reset)
+        git_commits_reset
         ;;
     *)
         ! [ -f "$1" ] \
