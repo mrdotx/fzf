406dd14a32ff98fa7c13a69797f07919a1be27a8
mrdotx <klassiker@gmx.de>
2022-05-18 14:02:40 +0200

    error handling added

--- fzf_git_commit.sh
+++ fzf_git_commit.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/fzf/fzf_git_commit.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/fzf
-# date:   2022-05-18T13:15:37+0200
+# date:   2022-05-18T14:01:41+0200
 
 # help
 script=$(basename "$0")
@@ -17,7 +17,7 @@ help="$script [-h/--help] -- script to show/checkout commits for a file
 git_commit() {
     git log --oneline -- "$1" \
         | fzf +s +m -e  \
-            --preview "git show --color $(printf "{1}" | cut -d' ' -f1)" \
+            --preview "git show --color $(printf "{1}" | cut -d" " -f1)" \
             --preview-window "right:70%" \
         | cut -d" " -f1
 }
@@ -27,7 +27,12 @@ case "$1" in
         printf "%s\n" "$help"
         ;;
     *)
-        [ -f "$1" ] \
-            && git checkout "$(git_commit "$1")" -- "$1"
+        ! [ -f "$1" ] \
+            && exit 1
+
+        commit_id="$(git_commit "$1")"
+
+        [ -n "$commit_id" ] \
+            && git checkout "$commit_id" -- "$1"
         ;;
 esac
