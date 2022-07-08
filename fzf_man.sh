b1ce0160eecbc30b737d80b0c62bc23bb1276eda
mrdotx <klassiker@gmx.de>
2022-07-08 19:40:34 +0200

    apropos removed

--- fzf_man.sh
+++ fzf_man.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/fzf/fzf_man.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/fzf
-# date:   2021-07-15T12:37:27+0200
+# date:   2022-07-08T19:23:38+0200
 
 # help
 script=$(basename "$0")
@@ -18,7 +18,7 @@ help="$script [-h/--help] -- script to search and open man pages
     && printf "%s\n" "$help" \
     && exit 0
 
-select=$(apropos -l '' \
+select=$(man -k -l '' \
     | cut -d ' ' -f1,2 \
     | sort \
     | fzf -m -e -i --preview "man {1}{2}" \
