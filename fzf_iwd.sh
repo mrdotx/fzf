0edd09f75e86d28cf251e0ea8a1e3eb5eb66a390
mrdotx <klassiker@gmx.de>
2023-05-21 10:29:02 +0200

    ssid security fix and structural optimizations

--- fzf_iwd.sh
+++ fzf_iwd.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/fzf/fzf_iwd.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/fzf
-# date:   2023-05-20T18:12:37+0200
+# date:   2023-05-21T10:21:29+0200
 
 remove_escape_sequences() {
     tail -n +5 \
@@ -23,11 +23,39 @@ read_secret() {
     printf "\n"
 }
 
+get_device_power() {
+    printf "%s" "$devices" \
+        | awk -v device="$1" '$1 == device {print $3}'
+}
+
+get_ssid_security() {
+    iwctl station "$1" get-networks \
+        | remove_escape_sequences \
+        | sed 's/>//' \
+        | awk -v ssid="$2" '$1 == ssid {print $2}'
+}
+
+scan_ssids() {
+    timer=3
+
+    iwctl station "$1" scan \
+        && printf "scan for ssids with device %s, please wait %s seconds\n" \
+            "$1" \
+            "$timer" \
+        && sleep $timer
+
+    ssids=$(iwctl station "$1" get-networks \
+        | remove_escape_sequences \
+        | sed 's/>//' \
+        | awk '{print $1}' \
+    )
+}
+
 connect_ssid() {
-    case "$1" in
+    case "$2" in
         open)
-            printf "try to connect to \"%s\"\n" "$ssid"
-            iwctl station "$device" connect "$ssid"
+            printf "try to connect to \"%s\"\n" "$3"
+            iwctl station "$1" connect "$3"
             ;;
         *)
             printf "enable password feedback [y]es/[N]o: " \
@@ -42,49 +70,18 @@ connect_ssid() {
                         read_secret -r psk
                         ;;
                 esac
-            printf "try to connect to \"%s\" with psk\n" "$ssid"
-            iwctl station "$device" connect "$ssid" -P "$psk"
+            printf "try to connect to \"%s\" with psk\n" "$3"
+            iwctl station "$1" connect "$3" -P "$psk"
             ;;
     esac
 }
 
-select_device_power() {
-    interface=$(iwctl device list \
-        | remove_escape_sequences \
-    )
-
-    device=$(printf "%s" "$interface" \
-        | awk '{print $1}' \
-        | fzf -e --cycle \
-            --preview-window "up:75%" \
-            --preview-label "[ toggle power ]" \
-            --preview "iwctl device {} show" \
-    )
-
-    power=$(printf "%s" "$interface" \
-        | grep "$device" \
-        | awk '{print $3}' \
-    )
-
-    [ -n "$device" ] \
-        && case $power in
-            on)
-                iwctl device "$device" set-property Powered off
-                ;;
-            off)
-                iwctl device "$device" set-property Powered on
-                ;;
-        esac
-
-    select_device
-}
-
 select_device() {
-    interface=$(iwctl device list \
+    devices=$(iwctl device list \
         | remove_escape_sequences \
     )
 
-    device=$(printf "%s" "$interface" \
+    device=$(printf "%s" "$devices" \
         | awk '{print $1"\n== toggle power =="}' \
         | fzf -e --cycle \
             --preview-window "up:75%" \
@@ -99,33 +96,34 @@ select_device() {
             esac" \
     )
 
-    power=$(printf "%s" "$interface" \
-        | grep "$device" \
-        | awk '{print $3}' \
-    )
-
     [ "$device" = "== toggle power ==" ] \
-        || [ "$power" = "off" ] \
+        || [ "$(get_device_power "$device")" = "off" ] \
         && select_device_power
 
     [ -n "$device" ] \
         || exit 1
 }
 
-scan_ssids() {
-    timer=3
-
-    iwctl station "$device" scan \
-        && printf "scan for ssids with device %s, please wait %s seconds\n" \
-            "$device" \
-            "$timer" \
-        && sleep $timer
-
-    ssids=$(iwctl station "$device" get-networks \
-        | remove_escape_sequences \
-        | sed 's/>//' \
+select_device_power() {
+    device_power=$(printf "%s" "$devices" \
         | awk '{print $1}' \
+        | fzf -e --cycle \
+            --preview-window "up:75%" \
+            --preview-label "[ toggle power ]" \
+            --preview "iwctl device {} show" \
     )
+
+    [ -n "$device_power" ] \
+        && case "$(get_device_power "$device_power")" in
+            on)
+                iwctl device "$device_power" set-property Powered off
+                ;;
+            off)
+                iwctl device "$device_power" set-property Powered on
+                ;;
+        esac
+
+    select_device
 }
 
 select_ssid() {
@@ -139,24 +137,18 @@ select_ssid() {
             --preview-label "[ connect ]" \
             --preview "iwctl known-networks {} show >/dev/null 2>&1 \
                         && iwctl known-networks {} show \
-                        || iwctl station \"$device\" get-networks" \
-    )
-
-    security=$(iwctl station "$device" get-networks \
-        | remove_escape_sequences \
-        | sed 's/>//' \
-        | awk '{print $2}' \
+                        || iwctl station \"$1\" get-networks" \
     )
 
     [ -n "$ssid" ] \
         && case "$ssid" in
             "== disconnect ==")
-                iwctl station "$device" disconnect
-                select_ssid
+                iwctl station "$1" disconnect
+                select_ssid "$1"
                 ;;
             "== rescan ==")
-                scan_ssids
-                select_ssid
+                scan_ssids "$1"
+                select_ssid "$1"
                 ;;
             *)
                 [ "$ssid" = "== enter ssid ==" ] \
@@ -164,12 +156,12 @@ select_ssid() {
                     && printf "ssid: " \
                     && read -r ssid
 
-                iwctl station "$device" disconnect
-                connect_ssid "$security"
+                iwctl station "$1" disconnect
+                connect_ssid "$1" "$(get_ssid_security "$1" "$ssid")" "$ssid"
                 ;;
         esac
 }
 
 select_device \
-    && scan_ssids \
-    && select_ssid
+    && scan_ssids "$device" \
+    && select_ssid "$device"
