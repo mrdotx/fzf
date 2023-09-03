828441bb4803c1d83acb0d0fc39f2e99c168d6c1
mrdotx <klassiker@gmx.de>
2023-09-03 10:06:33 +0200

    man errors removed from previews

--- fzf_man.sh
+++ fzf_man.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/fzf/fzf_man.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/fzf
-# date:   2023-06-29T21:48:29+0200
+# date:   2023-09-03T10:05:24+0200
 
 # speed up script and avoid language problems by using standard c
 LC_ALL=C
@@ -26,7 +26,7 @@ select=$(man -k -l '' \
     | sort \
     | fzf -e --query="^" \
         --preview-window "up:75%" \
-        --preview "man {1}{2}" \
+        --preview "man {1}{2} 2>/dev/null" \
     | cut -d ' ' -f1,2 \
     | tr -d ' ' \
 )
