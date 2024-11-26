024eca6412ef2f6162e55b67408b8023d2815a15
mrdotx <klassiker@gmx.de>
2024-11-26 06:44:10 +0100

    process special chars improved

--- fzf_bulkrename.sh
+++ fzf_bulkrename.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/fzf/fzf_bulkrename.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/fzf
-# date:   2024-10-21T08:15:14+0200
+# date:   2024-11-26T06:44:05+0100
 
 # speed up script and avoid language problems by using standard c
 LC_ALL=C
@@ -45,7 +45,8 @@ bulkrename() {
     cp -f "$cache_folder/$original_file" "$cache_folder/$modify_file"
     "$edit" "$cache_folder/$modify_file"
 
-    # create bulk rename script
+    # create bulk rename script and process special characters
+    # ' -> '\'' | *@#@* -> '
     printf "%b" \
         "#!/bin/sh\n\n" \
         "# This script will be executed when you close the editor.\n" \
@@ -53,9 +54,14 @@ bulkrename() {
             > "$cache_folder/$script_file"
 
     awk -v cmd="$cmd" 'NR==FNR { a[NR]=$0; next }
-        $0!=a[FNR] { print cmd" \\\n\t\""$0"\" \\\n\t\""a[FNR]"\"" }' \
+        $0!=a[FNR] { print cmd" \\\n\t*@#@*"$0"*@#@* \\\n\t*@#@*"a[FNR]"*@#@*" }' \
         "$cache_folder/$modify_file" "$cache_folder/$original_file" \
             >> "$cache_folder/$script_file"
+
+    sed \
+        -i -e "s/'/\\'\\\'\'/g" \
+        -i -e "s/\*@#@\*/'/g" \
+        "$cache_folder/$script_file"
 
     # check bulk rename script
     "$edit" "$cache_folder/$script_file"
